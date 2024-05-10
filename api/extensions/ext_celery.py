452458c9f38070168f40479341e5b08894e764a5
qcloud <ubuntu@localhost.localdomain>
2024-05-10 09:28:07 +0800

    fix time

--- api/extensions/ext_celery.py
+++ api/extensions/ext_celery.py
@@ -45,7 +45,7 @@ def init_app(app: Flask) -> Celery:
     beat_schedule = {
         "crawl_from_rsshub_task": {
             "task": "schedule.crawl_from_rsshub_task.crawl_from_rsshub_task",
-            "schedule": crontab(minute=0, hour="0, 6, 8, 10, 12, 14, 16, 18, 20"),
+            "schedule": crontab(minute=0, hour="0, 9, 15, 21"),
         }
     }
     celery_app.conf.update(beat_schedule=beat_schedule, imports=imports, timezone="Asia/Shanghai", enable_utc=False)
