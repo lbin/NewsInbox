c721288e10a838845f1a4cf6427a81cd7d33c3fe
qcloud <ubuntu@localhost.localdomain>
2024-05-22 14:58:47 +0800

    update autorun

--- api/schedule/daily_summary_task.py
+++ api/schedule/daily_summary_task.py
@@ -1,28 +1,29 @@
 import datetime
-import os
 import glob
-from .logger import logger
-import app
 import json
+import os
+
+import app
+
+from .logger import logger
 
 
 def get_current_data():
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
     folder_path = f"./schedule/data/{current_date}/focus"
-    
-    
+
     json_files = []
     for file in glob.glob(os.path.join(folder_path, "*.json")):
         with open(file) as f:
-            json_data = f.read()
+            json_data = json.load(f)
             json_files.append(json_data)
-            
-    merged_json = json.dumps(json_files)
+
+    merged_json = json.dumps(json_files, ensure_ascii=False, indent=4)
     return merged_json
+
 
 @app.celery.task
 def daily_summary_task():
     logger.info("daily_summary_task")
     json_files = get_current_data()
-    
-
+    logger.info(f"{json_files}")
--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,4 @@
 cd NewsInbox/wechat-agent/
-./stop.sh
 ./start.sh
 sleep 2
 cat wechat_robot.log
